41d0c002222d24468945a4c58e61be58baba9d0f
ahqmrf <ahqmrf@gmail.com>
2017-04-22 01:47:24 +0600

    Base activity added

---
+++ app/src/main/java/apps/ahqmrf/mock/BaseActivity.java
@@ -0,0 +1,74 @@
+package apps.ahqmrf.mock;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import butterknife.BindView;
+
+/**
+ * Created by bsse0 on 4/22/2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Nullable @BindView(R.id.app_toolbar) protected Toolbar toolbar;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+    }
+
+    protected void setToolbarWithBackArrow() {
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.base, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setToolbarTitle();
+        onViewCreated();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void trigger(Class<?> activityClass) {
+        if (!this.getClass().equals(activityClass)) {
+            Intent intent = new Intent(this, activityClass);
+            startActivity(intent);
+        }
+    }
+
+    protected abstract void setToolbarTitle();
+    protected abstract void onViewCreated();
+
+}

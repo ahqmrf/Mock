6c8ff8566569626b0eda5787b8dab7ac2ac0ccdb
ahqmrf <ahqmrf@gmail.com>
2017-04-24 02:18:13 +0600

    created package

--- app/src/main/java/apps/ahqmrf/mock/activity/MainActivity.java
+++ app/src/main/java/apps/ahqmrf/mock/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package apps.ahqmrf.mock;
+package apps.ahqmrf.mock.activity;
 
 import android.os.Bundle;
 
+import apps.ahqmrf.mock.BaseActivity;
+import apps.ahqmrf.mock.R;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {

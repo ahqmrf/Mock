260f03fde7e65893ca13929c225a64b9018d1b04
ahqmrf <ahqmrf@gmail.com>
2017-05-01 17:48:38 +0600

    fixed pp problem

--- app/src/main/java/apps/ahqmrf/mock/activity/SettingsActivity.java
+++ app/src/main/java/apps/ahqmrf/mock/activity/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,12 +27,12 @@ public class SettingsActivity extends BaseActivity {
     @BindString(R.string.title_settings)
     String title;
 
-    @BindView(R.id.image_main) ImageView mImageView;
-    @BindView(R.id.text_name) TextView mName;
-    @BindView(R.id.layout_progress) View progressLayout;
-    @BindView(R.id.text_email) TextView mEmail;
-    @BindView(R.id.text_full_name) TextView mFullName;
-    @BindView(R.id.text_username) TextView mUsername;
+    @BindView(R.id.image_main)      ImageView mImageView;
+    @BindView(R.id.text_name)       TextView  mName;
+    @BindView(R.id.layout_progress) View      progressLayout;
+    @BindView(R.id.text_email)      TextView  mEmail;
+    @BindView(R.id.text_full_name)  TextView  mFullName;
+    @BindView(R.id.text_username)   TextView  mUsername;
 
     private StorageReference mStorage = FirebaseStorage.getInstance().getReference();
     private String username, imageUrl, fullName, email;
@@ -60,22 +61,24 @@ public class SettingsActivity extends BaseActivity {
         mEmail.setText(email);
         mFullName.setText(fullName);
         mUsername.setText(username);
-
-        StorageReference photoStorage = mStorage.child(username).child(Const.Keys.PROFILE_PIC).child(imageUrl);
-        final long ONE_MEGABYTE = 1024 * 1024;
-        photoStorage.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                mImageView.setImageBitmap(bitmap);
-                progressLayout.setVisibility(View.GONE);
-            }
-        })
-        .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                progressLayout.setVisibility(View.GONE);
-            }
-        });
+        if (TextUtils.isEmpty(imageUrl)) {
+            progressLayout.setVisibility(View.GONE);
+        } else {
+            StorageReference photoStorage = mStorage.child(username).child(Const.Keys.PROFILE_PIC).child(imageUrl);
+            final long ONE_MEGABYTE = 1024 * 1024;
+            photoStorage.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    mImageView.setImageBitmap(bitmap);
+                    progressLayout.setVisibility(View.GONE);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressLayout.setVisibility(View.GONE);
+                }
+            });
+        }
     }
 }

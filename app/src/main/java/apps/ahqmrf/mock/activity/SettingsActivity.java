9eefda5eb553790616b608a0409204502358199d
ahqmrf <ahqmrf@gmail.com>
2017-05-01 17:21:44 +0600

    updated settings

--- app/src/main/java/apps/ahqmrf/mock/activity/SettingsActivity.java
+++ app/src/main/java/apps/ahqmrf/mock/activity/SettingsActivity.java
@@ -3,10 +3,13 @@ package apps.ahqmrf.mock.activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -25,6 +28,10 @@ public class SettingsActivity extends BaseActivity {
 
     @BindView(R.id.image_main) ImageView mImageView;
     @BindView(R.id.text_name) TextView mName;
+    @BindView(R.id.layout_progress) View progressLayout;
+    @BindView(R.id.text_email) TextView mEmail;
+    @BindView(R.id.text_full_name) TextView mFullName;
+    @BindView(R.id.text_username) TextView mUsername;
 
     private StorageReference mStorage = FirebaseStorage.getInstance().getReference();
     private String username, imageUrl, fullName, email;
@@ -50,6 +57,9 @@ public class SettingsActivity extends BaseActivity {
         email = Utility.getString(this, Const.Keys.EMAIL);
 
         mName.setText(fullName);
+        mEmail.setText(email);
+        mFullName.setText(fullName);
+        mUsername.setText(username);
 
         StorageReference photoStorage = mStorage.child(username).child(Const.Keys.PROFILE_PIC).child(imageUrl);
         final long ONE_MEGABYTE = 1024 * 1024;
@@ -58,6 +68,13 @@ public class SettingsActivity extends BaseActivity {
             public void onSuccess(byte[] bytes) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                 mImageView.setImageBitmap(bitmap);
+                progressLayout.setVisibility(View.GONE);
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressLayout.setVisibility(View.GONE);
             }
         });
     }
